5b6dbced079ed5b3cb7acb8c76c8206f554fd413
apotato369550 <johnandresyap510@gmail.com>
2021-03-08 07:44:12 +0800

    Add Grading Students

---
+++ Hackerrank/Algorithms/Grading Students/GradingStudents.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        // search up list methods
+        List<Integer> roundedGrades = new ArrayList<Integer>();
+        
+        for(int i = 0; i < grades.size(); i++){
+            int score = grades.get(i);
+            if(score >= 38){
+                // the conditional here is what's wrong
+                if(score + (5 - (score % 5)) - score < 3){
+                    roundedGrades.add(score + (5 - (score % 5)));
+                } else {
+                    roundedGrades.add(score);
+                }
+            } else {
+                roundedGrades.add(score);
+            }
+        }
+        
+        return roundedGrades;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = new ArrayList<>();
+
+        for (int i = 0; i < gradesCount; i++) {
+            int gradesItem = Integer.parseInt(bufferedReader.readLine().trim());
+            grades.add(gradesItem);
+        }
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
