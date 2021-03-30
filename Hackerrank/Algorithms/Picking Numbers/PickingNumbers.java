9fb71517fb1516b4729b13138c4752ff18342e36
apotato369550 <johnandresyap510@gmail.com>
2021-03-30 08:01:45 +0800

    Add Picking Numbers

---
+++ Hackerrank/Algorithms/Picking Numbers/PickingNumbers.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+        int[] frequency = new int[100];
+        int longest = 0;
+        
+        for(int value : a){
+            frequency[value]++;
+        }
+        
+        for(int i = 0; i < a.size() - 1; i++){
+            int subArray = frequency[i] + frequency[i + 1];
+            if(subArray > longest){
+                longest = subArray;
+            }
+        }
+        
+        return longest;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
