d364983b48992d3e89a871602ddb3c7fa5fd3dd4
apotato369550 <johnandresyap510@gmail.com>
2021-05-12 07:16:22 +0800

    Add You're a Square

---
+++ Codewars/7 Kyu/You're a square/Square.java
@@ -0,0 +1,6 @@
+public class Square {    
+    public static boolean isSquare(int n) {  
+        double square = Math.sqrt(n);
+        return (square - (Math.floor(square)) == 0); // fix me!
+    }
+}
