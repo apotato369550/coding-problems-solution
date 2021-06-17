5a0d6903a421a77d1f2c71f5a0bbccd4f7218db5
apotato369550 <johnandresyap510@gmail.com>
2021-06-17 18:53:09 +0800

    Add alternate solution 3

--- Hackerrank/Algorithms/Bigger is Greater/Bigger2.java
+++ Hackerrank/Algorithms/Bigger is Greater/Bigger2.java
@@ -23,8 +23,10 @@ class Result {
     // traverse through the string in reverse order
     // if w[i] < w[i + 1] then split w into two substrings
     // take the substring on the right and find the smallest character lexicographically
-    // swap that character with w[i]
+    // swap that character with w[i] (last char)
     // sort the second substring lexicographically increasing
+    // do this in the morning
+    // re-watch the video
         if(w.length() <= 2){
             StringBuilder reverse = new StringBuilder(w).reverse();
             if(w.compareTo(reverse.toString()) < 0){
@@ -39,16 +41,33 @@ class Result {
         for(int i = w.length() - 2; i > 0; i--){
             char current = w.charAt(i);
             char previous = w.charAt(i + 1);
-            if(current.compareTo(previous) < 0){
+            if(Character.valueOf(current).compareTo(Character.valueOf(previous)) < 0){
                 split = i;
                 break;
             }
         }
         
-        String left = w.substring(0, i + 1);
-        String right = w.substring(i);
+        String left = w.substring(0, split + 1);
+        String right = w.substring(split);
+        
+        System.out.println(w);
+        System.out.println(left + " " + right);
+        
+        // sort this out
+        // this convert this to char array
+        char charArray[] = right.toCharArray();
+        Arrays.sort(charArray);
+        char smallest = charArray[0];
+        
+        char last = left.charAt(left.length() - 1);
+        
+        left = left.substring(0, left.length() - 1) + smallest;
+        right = last + right.substring(1);
+        System.out.println(left + " " + right);
+        // check if this works??
         
         // continue work here
+        return "lmlao";
     }
 
 }
---
+++ Hackerrank/Algorithms/Bigger is Greater/Bigger3.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'biggerIsGreater' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING w as parameter.
+     */
+
+    public static String biggerIsGreater(String w) {
+        if(w.length() == 1){
+            return "no answer";
+        }
+        
+        int maxLex1 = 0;
+        int maxLex2 = 0;
+        // this is a weird thing???
+        for(int j = 1; j < word.length(); j++){
+            if(word.charAt(j) > word.charAt(j - 1)){
+                maxLex1 = (j > maxLex1) ? j : maxLex1;
+            }
+        }
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                String w = bufferedReader.readLine();
+
+                String result = Result.biggerIsGreater(w);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
