716bd498a5d79156c3badd8bb8618d3b02c64902
apotato369550 <johnandresyap510@gmail.com>
2021-07-15 18:53:17 +0800

    Work on Lisa2 Solution

--- Hackerrank/Algorithms/Lisa's Workbook/Lisa2.java
+++ Hackerrank/Algorithms/Lisa's Workbook/Lisa2.java
@@ -20,7 +20,22 @@ class Result {
 
     public static int workbook(int n, int k, List<Integer> arr) {
     // Write your code here
+        int result = 0;
+        int page = 1;
 
+        for(int i = 0; i < arr.size(); i++){
+            int problems = arr.get(i);
+            for(int j = 1; j < problems; j++){
+                if(j == page){
+                    result++;
+                }
+                if(j % k == 0 || j == problems){
+                    page++;
+                }
+            }
+        }
+
+        return result;
     }
 
 }
